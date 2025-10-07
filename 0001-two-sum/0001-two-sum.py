6ddfab6067872dd5c0f3ac409136366c1bab048b
Sai Satwik Bikumandla <saisatwikbikumandla9@gmail.com>
2025-10-07 11:31:07 -0400

    Time: 0 ms (100%), Space: 18.7 MB (62.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        result = []
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-
-            
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        hash_map = {} #index, value
+
+        for index, value in enumerate(nums):
+
+            if target - value in hash_map:
+                return index, hash_map[target-value]
+            else:
+                hash_map[value] = index   
